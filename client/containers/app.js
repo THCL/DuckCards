97699a10f91a9bbebd2bdffcf29773087c2bd8a3
llo1 <louislo@gmail.com>
2016-07-20 20:50:35 -0700

    Render auth component navbar

--- client/containers/app.js
+++ client/containers/app.js
@@ -1,13 +1,48 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { createStore } from 'redux';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import React, { Component, PropTypes} from 'react'
+import { render } from 'react-dom'
+import { connect } from 'react-redux'
+import { loginUser } from '../actions'
+import Login from '../components/Login'
+import Navbar from '../components/Navbar'
+
 import Main from './Main'; // Our custom react component
 
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
 
-// Render the main app react component into the app div.
-// For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
-render(<Main />, document.getElementById('app'));
+class App extends Component {
+  render() {
+    const { dispatch, quote, isAuthenticated, errorMessage } = this.props
+    return (
+      <div>
+        <Navbar
+          isAuthenticated={isAuthenticated}
+          errorMessage={errorMessage}
+          dispatch={dispatch}
+        />
+        <main />
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired,
+  errorMessage: PropTypes.string
+}
+
+function mapStateToProps(state) {
+  const { auth } = state
+  const { isAuthenticated, errorMessage } = auth
+
+  return {
+    isAuthenticated,
+    errorMessage
+  }
+}
+
+export default connect(mapStateToProps)(App)
+
+
+// // Render the main app react component into the app div.
+// // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
+// render(<Main />, document.getElementById('app'));
